a1ec92c230cb27ac4e27393dadbabb66ce0c98a4
Gabriel Pires <gabpiresdev@gmail.com>
2025-02-05 19:47:21 -0300

    add Zod validation error handling and update Fastify server configuration

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,12 +1,34 @@
-import { env } from '@/env'
 import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
 
-server.register(fastifyCors, { origin: '*' })
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
 
-console.log(env.DATABASE_URL)
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.validation,
+    })
+  }
+
+  // Send the error to a observability service
+
+  console.error(error)
+
+  return reply.status(500).send({
+    message: 'Internal server error.',
+  })
+})
+
+server.register(fastifyCors, { origin: '*' })
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
